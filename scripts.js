47f6043d6035346c682b509dbb937e40161c4be5
Will Garvey <willmgarvey@gmail.com>
2024-04-21 15:57:01 -0500

    modified script to adjust to live

--- scripts.js
+++ scripts.js
@@ -16,16 +16,9 @@ document.addEventListener('DOMContentLoaded', function () {
     let sidebarDeleted = document.getElementById('selected-deleted');
     let sidebarLineID = document.getElementById('selected-id');
 
-    let previousSpeakerElement = document.getElementById('previous-speaker'); // Context area
-    let previousLineElement = document.getElementById('previous-line');
-    let currentSpeakerElement = document.getElementById('current-speaker');
-    let currentLineElement = document.getElementById('current-line');
-    let nextSpeakerElement = document.getElementById('next-speaker');
-    let nextLineElement = document.getElementById('next-line');
+    let contextList = document.querySelector('.context-list'); // Context area
 
-    let lowerButtonUp = document.getElementById('lower-button-up'); // Buttons to browse context area
-    let lowerButtonDown = document.getElementById('lower-button-down');
-
+    // Retrieve the search text and selected character from the form
     let searchText = searchInput.value;
     populateCharacterOptions();
 
@@ -37,18 +30,11 @@ document.addEventListener('DOMContentLoaded', function () {
         searchText = searchInput.value;
     })
 
-    lowerButtonUp.addEventListener('click', function () { // Press up arrow icon
-        assignContextResults("up"); 
-    });
-
-    lowerButtonDown.addEventListener('click', function () { // Press down arrow icon
-        assignContextResults("down");    
-    });
     document.getElementById('searchButton').addEventListener('click', function (searchText) { // Press the Search button
 
         
         // Retrieve the search text and selected character from the form
-        searchText = searchInput.value;
+        searchText = searchInput.value.toLowerCase();
         let selectedCharacter = dropdown.value;
 
         // Create an instance of the Search class
@@ -71,56 +57,6 @@ document.addEventListener('DOMContentLoaded', function () {
             return lines;
             }
     });
-    // function to assign script lines to the bottom half of the screen
-    function assignContextResults(button) {
-        let currentLineID = parseInt(sidebarLineID.textContent);
-            if (currentLineID >= 1) {
-                if (button === "up")
-                {
-                    currentSpeakerElement.innerHTML = scriptLines[currentLineID - 1].Speaker;
-                    currentLineElement.innerHTML = scriptLines[currentLineID - 1].LineText;
-
-                    nextSpeakerElement.innerHTML = scriptLines[currentLineID].Speaker;
-                    nextLineElement.innerHTML = scriptLines[currentLineID].LineText;
-
-                    try {
-                        previousSpeakerElement.innerHTML = scriptLines[currentLineID - 2].Speaker;
-                        previousLineElement.innerHTML = scriptLines[currentLineID - 2].LineText;
-                    }
-
-                    catch {
-                        previousSpeakerElement.innerHTML = "";
-                        previousLineElement.innerHTML = "<b>Beginning of Scene<//b>";
-                        sidebarLineID.textContent = currentLineID + 1;
-                    }
-
-                    sidebarLineID.textContent = currentLineID - 1;
-                }
-
-                else if (button === "down")
-                {
-                    currentSpeakerElement.innerHTML = scriptLines[currentLineID + 1].Speaker;
-                    currentLineElement.innerHTML = scriptLines[currentLineID + 1].LineText;
-
-                    try {
-                        nextSpeakerElement.innerHTML = scriptLines[currentLineID + 2].Speaker;
-                        nextLineElement.innerHTML = scriptLines[currentLineID + 2].LineText;
-                    }
-
-                    catch {
-                        nextSpeakerElement.innerHTML = "";
-                        nextLineElement.innerHTML = "<b>End of Scene<//b>";
-                        sidebarLineID.textContent = currentLineID - 1;
-                    }
-
-
-                    previousSpeakerElement.innerHTML = scriptLines[currentLineID].Speaker;
-                    previousLineElement.innerHTML = scriptLines[currentLineID].LineText;
-
-                    sidebarLineID.textContent = currentLineID + 1;
-                }
-            }
-    }
     // function to make matching wordsin results show as bold
     function addStrongTags(sentence, wordsToWrap) {
         const words = sentence.split(' ');
@@ -133,8 +69,7 @@ document.addEventListener('DOMContentLoaded', function () {
           return word;
         });
         return wrappedWords.join(' ');
-      }
-      
+      } 
     // function to assign script lines the to results list
     function assignSearchResults(lines){       
         if (lines === null || lines === 'Invalid' || lines.length == 0) { //No results
@@ -145,14 +80,7 @@ document.addEventListener('DOMContentLoaded', function () {
             paragraph.className = "result-zero";
             paragraph.appendChild(bold);
             resultList.appendChild(paragraph);
-            
-            // Clear out context menu
-            previousLineElement.textContent = '';
-            previousSpeakerElement.textContent = '';
-            currentLineElement.textContent = '';
-            currentSpeakerElement.textContent = '';
-            nextLineElement.textContent = '';
-            nextSpeakerElement.textContent = '';
+            //sidebarSeason.textContent = searchText;
         }
 
         else { // write script lines to the list           
@@ -183,31 +111,24 @@ document.addEventListener('DOMContentLoaded', function () {
                     sidebarScene.textContent = selectedItem.Scene;
                     sidebarCharacter.textContent = selectedItem.Speaker;
                     sidebarDiskNumber.textContent = selectedItem.Disk;
-                    sidebarDeleted.textContent = selectedItem.IsDeleted;
+                    sidebarDeleted.textContent = selectedItem.IsDeleted ? "YES" : "NO";
                     sidebarLineID.textContent = selectedItem.LineID;
 
-                    let selectedLine = scriptLines.find(obj => obj.LineID === parseInt(sidebarLineID.textContent));
-                    let previousLine = scriptLines.find(obj => obj.LineID === parseInt(sidebarLineID.textContent) - 1);
-                    let nextLine = scriptLines.find(obj => obj.LineID === parseInt(sidebarLineID.textContent) + 1);
+                    let selectedLine = scriptLines.find(obj => obj.LineID === parseInt(sidebarLineID.textContent)); //TODO: Use this elsewhere
 
-                    let selectedSpeaker = selectedLine.Speaker; // Select the speaker
-                    let selectedLineText = selectedLine.LineText; // Select the line
-
-                    // Retrieve the Speaker and LineText properties for the previous line
-                    let previousSpeaker = previousLine ? previousLine.Speaker : "";
-                    let previousLineText = previousLine ? previousLine.LineText : "";
-
-                    // Retrieve the Speaker and LineText properties for the next line
-                    let nextSpeaker = nextLine ? nextLine.Speaker : "";
-                    let nextLineText = nextLine ? nextLine.LineText : "";
-
-                    // Assign values to the elements
-                    previousSpeakerElement.textContent = previousSpeaker;
-                    previousLineElement.textContent = previousLineText;
-                    currentSpeakerElement.textContent = selectedSpeaker;
-                    currentLineElement.textContent = selectedLineText;
-                    nextSpeakerElement.textContent = nextSpeaker;
-                    nextLineElement.textContent = nextLineText;
+                    // Assign values to the  context elements
+                    contextList.innerHTML = "";
+                    for (let i = -2; i<3; i++){
+                        parseInt(selectedLine.LineID) + i
+                        let line = scriptLines.find(obj => obj.LineID === parseInt(sidebarLineID.textContent) + i);
+                        let className = "context-item";
+                        let lineText = addStrongTags(line.LineText, searchInput.value);
+                        let speaker = line.Speaker;
+                        let li = document.createElement('li');
+                        li.className = className;
+                        li.innerHTML = '<p><b>' + speaker + "</b>: " + lineText + '</p>';
+                        contextList.appendChild(li);
+                    }
                 });
                 resultList.appendChild(li);
                 listCount++;
@@ -242,4 +163,3 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     }
 });
-
